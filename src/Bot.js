6b306eec93808ac1f56f4861ead54aa1ade3c6e4
Quincy Williams <QLWilliams@student.fullsail.edu>
2022-06-10 23:49:53 -0400

    Cleaned up Bot.js and added Utils.js to refactor

--- src/Bot.js
+++ src/Bot.js
@@ -1,3 +1,6 @@
+const { listCharacters, listMoveTypes, printHelpText, listAliases } = require("./Utils.js");
+const { characters, characterAliases } = require("./Utils.js");
+
 const { Client, Intents } = require("discord.js");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
 
@@ -5,43 +8,9 @@ const config = require('../config.json');
 const prefix = config.prefix;
 
 
-//Character name arrays - Holds the names of each character currently in the game
-const characters = ['Anji Mito', 'Axl Low', 'Baiken', 'Chipp Zanuff', 'Faust', 'Giovanna', 'Goldlewis', 'Happy Chaos',
-'I-No', 'Jack-O', 'Ky Kiske', 'Leo WhiteFang', 'May', 
-'Millia Rage', 'Nagoriyuki', 'Potemkin', 'Ramlethal', 'Testament', 'Zato-1'];
-const characterAliases = ['Anji', 'Axl', 'Baiken', 'Chipp', 'Gio', 'Golddick', 'GoldDick', 'Chaos', 'Ky', 'Leo', 'Millia', 'Nago',
-'Pot', 'Ram', 'Zato'];
-const moveTypes = ['Normal Moves', 'Special Moves', 'Supers', 'Other'];
-
 //Commands to get character list
 const charCommands = ['Characters', 'Chars', 'characters', 'chars'];
 
-//Help string presented to the user when $help or $Help is entered
-const helpString = ["- All commands must be prefixed with a $ to get a response.",
-"- Currently, I only provide data for GGST but that may to change in the future.",
-"- Enter a character name or alias from the game for a general overview of the character.",
-"- Enter $aliases or $Aliases to list accepted aliases for characters in the game", 
-"- Enter a character name with a move to get details about the move.",
-"- Enter the characters name or alias and moves (Ex: $Nagoriyuki moves) and their moveset will be listed",
-"- Enter the character name or alias and the type of of (Ex: $Axl Supers) to get that characters overdrives.",
-"- Enter command $movetypes to get the types of moves that are available for each character."]
-
-//Printing functions
-const listCharacters = () => {
-    return characters.join('\n');
-}
-
-const listMoveTypes = () => {
-    return moveTypes.join('\n');
-}
-
-const printHelpText = () => {
-    return helpString.join('\n');
-}
-
-const listAliases = () => {
-    return characterAliases.join('\n');
-}
 
 //When the client is ready, indicate in console that the bot is logged in
 client.on("ready", () => {
@@ -52,19 +21,25 @@ client.on("ready", () => {
 client.on("messageCreate", msg => {
     let messageBuffer = "" // Used to store entire string for comparing request and command to determine how the bot shoud response
     let messageCommand = "";  // To check the value after the '$' to determine how to proceed
-    let messageRequest = ""; // To check if there is a specific value after the inital message string to check if work needs to be done
+    let messageRequest = ""; // To check if there is a specific value after the inital message string to determine if work needs to be done
 
     if(!msg.content.startsWith(prefix) || msg.author.bot) {
         return;
     }
     else if (msg.content.startsWith(prefix)) {
         messageBuffer = msg.content;
-        messageCommand = messageBuffer.substring(messageBuffer.indexOf('$') + 1, messageBuffer.indexOf(' '));
-        messageRequest = messageBuffer.substring(messageBuffer.indexOf(' ') + 1);
+
+        if (messageBuffer.includes(' ')) {
+            messageCommand = messageBuffer.substring(messageBuffer.indexOf('$') + 1, messageBuffer.indexOf(' '));
+            messageRequest = messageBuffer.substring(messageBuffer.indexOf(' ') + 1);
+        } else {
+            messageCommand = messageBuffer.substring(messageBuffer.indexOf('$') + 1, messageBuffer.length);
+            messageRequest = "";
+        }
         
         //Check to make sure that some value was entered after the dollar sign
         if(messageCommand === "") {
-            msg.channel.send("Did you mean to enter a command? Try '$help");
+            msg.channel.send("Did you mean to enter a command? Try $help for options");
             return;
         }
         
@@ -75,19 +50,39 @@ client.on("messageCreate", msg => {
                     return;
                 }
             }
+
+            //Placeholder code for scraping data for each character
+            for(i = 0; i < characters.length; i++) {
+                for (j = 0; j < characterAliases.length; j++) {
+                    if(messageCommand === characters[i]) {
+                        msg.channel.send(characters[i]);
+                        return;
+                    } else if (messageCommand === characterAliases[j]) {
+                        msg.channel.send(characterAliases[j]);
+                        return;
+                    }
+                }
+            }
             
             if(messageCommand === "help" || messageCommand === "Help") {
                 msg.channel.send(printHelpText());
                 return;
             }
 
-            if(messageCommand === "movetypes"){
+            if(messageCommand === "movetypes" && messageRequest === ""){
                 msg.channel.send(listMoveTypes());
                 return;
             }
+            else if (messageCommand === "movetypes" && messageRequest !== "") {
+                msg.channel.send("Functionality coming soon");
+                return;
+            }
 
-            if(messageCommand === "aliases" || messageCommand === "Aliases") {
+            if(messageRequest === "" && (messageCommand === "aliases" || messageCommand === "Aliases")) {
                 msg.channel.send(listAliases());
+                return;
+            } else {
+                msg.channel.send("Not a valid command.") //User entered too many arguments
                 return;
             }
         }
---
+++ src/Utils.js
@@ -0,0 +1,40 @@
+/* File created to house functions and variables that can be used throughout the code
+without cluttering files. These functions should be imported and pass data that is added here */
+
+
+//Help string presented to the user when $help or $Help is entered
+const helpString = ["- All commands must be prefixed with a $ to get a response.",
+"- Currently, I only provide data for GGST but that may to change in the future.",
+"- Enter a character name or alias from the game for a general overview of the character.",
+"- Enter $aliases or $Aliases to list accepted aliases for characters in the game", 
+"- Enter a character name with a move to get details about the move.",
+"- Enter the characters name or alias and moves (Ex: $Nagoriyuki moves) and their moveset will be listed",
+"- Enter the character name or alias and the type of of (Ex: $Axl Supers) to get that characters overdrives.",
+"- Enter command $movetypes to get the types of moves that are available for each character."];
+
+//Character name arrays - Holds the names of each character currently in the game
+const characters = ['Anji Mito', 'Axl Low', 'Baiken', 'Chipp Zanuff', 'Faust', 'Giovanna', 'Goldlewis', 'Happy Chaos',
+'I-No', 'Jack-O', 'Ky Kiske', 'Leo WhiteFang', 'May', 
+'Millia Rage', 'Nagoriyuki', 'Potemkin', 'Ramlethal', 'Testament', 'Zato-1'];
+const characterAliases = ['Anji', 'Axl', 'Baiken', 'Chipp', 'Gio', 'Golddick', 'GoldDick', 'Chaos', 'Ky', 'Leo', 'Millia', 'Nago',
+'Pot', 'Ram', 'Zato'];
+const moveTypes = ['Normal Moves', 'Special Moves', 'Supers', 'Other'];
+
+
+const listCharacters = () => {
+    return characters.join('\n');
+}
+
+const listMoveTypes = () => {
+    return moveTypes.join('\n');
+}
+
+const printHelpText = () => {
+    return helpString.join('\n');
+}
+
+const listAliases = () => {
+    return characterAliases.join('\n');
+}
+
+module.exports = { listCharacters, listMoveTypes, printHelpText, listAliases, characters, characterAliases };
